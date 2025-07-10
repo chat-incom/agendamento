e43b2e071686e13e687a0335f1eb02da9ce0d01b
chat-incom <incom.slz@gmail.com>
2025-07-09 22:16:51 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+console.log('main.tsx initialized at', new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' }));
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
