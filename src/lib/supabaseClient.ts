a0f38857314a693e0a34dbb766774b3e97460414
chat-incom <incom.slz@gmail.com>
2025-08-21 16:54:13 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,13 +1,16 @@
+// lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-// Pega as variáveis de ambiente definidas no .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Verifica se as variáveis estão definidas
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Variáveis de ambiente do Supabase não configuradas!');
 }
 
-// Cria e exporta o client do Supabase
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+});
